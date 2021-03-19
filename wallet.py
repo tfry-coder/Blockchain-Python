a80004021c43ac3e32f0b7cd41239efe44be6aa7
tfry-coder <71101118+tfry-coder@users.noreply.github.com>
2021-03-18 20:21:09 -0400

    working code

--- wallet.py
+++ wallet.py
@@ -4,8 +4,23 @@ import os
 # import bit
 # print(bit.__version__)
 from bit import Key
+from bit import PrivateKeyTestnet
+from bit.net
+
+
+work import NetworkAPI
+
 from constants import *
 
+from web3 import Web3, Account, middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3.middleware import geth_poa_middleware
+
+w3 = Web3(Web3.HTTPProvider("http://localhost:8545"))
+
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
 
 mnemonic = os.getenv('MNEMONIC', "wisdom knock poet solution adult save alcohol close fruit razor win almost")
 coin = BTC
@@ -22,6 +37,51 @@ def derive_wallets(mnemonic, coin, numderive):
     return json.loads(output)
 
 
+def priv_key_to_account(coin, priv_key):
+    if coin == 'eth':
+        return Account.privateKeyToAccount(priv_key)
+    else:
+        return PrivateKeyTestnet(priv_key)
+
+eth_account = priv_key_to_account(ETH,eth_key)
+
+    
+btc_account = priv_key_to_account(BTCTEST,btc_key)
+
+def create_tx(coin,account,recipient,amount):
+    global tx_data
+    if coin =='eth':
+        value = w3.toWei(amount, "ether")
+        gasEstimate = w3.eth.estimateGas(
+            {"from": account, "to": recipient, "value": value}
+        )
+        tx_data = {
+            "from": account,
+            "to": recipient,
+            "value": amount,
+            "gasPrice": w3.eth.generateGasPrice(),
+            "gas": gasEstimate,
+            "nonce": w3.eth.getTransactionCount(account),
+            "chainId": w3.eth.chain_id
+        }
+        return tx_data
+    else:
+        tx_data = PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)])
+        return tx_data
+
+
+
+def send_tx(coin, account, recipient, amount):
+    tx = create_tx(coin, account, recipient, amount)
+    signed_tx = account.sign_transaction(tx)
+    if coin == ETH:
+        result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        print(result)
+        return result
+    else:
+        result = NetworkAPI.broadcast_tx_testnet(signed_tx)
+        return result
+
 coins = {
     BTCTEST: derive_wallets(mnemonic, BTCTEST, numderive),
     ETH: derive_wallets(mnemonic, ETH, numderive)
@@ -31,54 +91,15 @@ coins = {
 print(json.dumps(coins, indent=4))
 
 
-eth_key = coins['eth'][1]['privkey']
-btc_key = coins['btc-test'][0]['privkey']
+# eth_key = coins['eth'][1]['privkey']
+# btc_key = coins['btc-test'][0]['privkey']
+# print("eth_key", eth_key)
+# print("btc_key", btc_key)
+# eth_to_acc = coins['eth'][0]['address']
+# btc_to_acc = coins['btc-test'][1]['address']
+# print("eth_to_acc", eth_to_acc)
+# print("btc_to_acc", btc_to_acc)
 
-def priv_key_to_account(coin, priv_key):
-    global account
-    if coin == 'eth':
-        account=Account.privateKeyToAccount(priv_key)
-    else:
-        account=PrivateKeyTestnet(priv_key)
-    return account
-
-priv_key_to_account(ETH,eth_key)
-
-    
-priv_key_to_account(BTCTEST,btc_key)
-
-def create_tx(coin,account,recipient,amount):
-    global tx_data
-    if coin =='eth':
-        gasEstimate = w3.eth.estimateGas(
-            {"from": account.address, "to": recipient, "value": amount}
-        )
-        tx_data= {
-            "from": account.address,
-            "to": recipient,
-            "value": amount,
-            "gasPrice": w3.eth.gasPrice,
-            "gas": gasEstimate,
-            "nonce": w3.eth.getTransactionCount(account.address),
-        }
-        return tx_data
-    else:
-        tx_data = PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)])
-        return tx_data
-
-create_tx(BTCTEST,account,'moXSjCRnNxXK1njCsdVaSUz2zR7La8KPat',0.0001)
-
-def send_tx(coin, account, recipient, amount):
-    if coin == ETH:
-        tx = create_tx(coin,account, recipient, amount)
-        signed_tx = account.sign_transaction(tx)
-        result = w3.eth.sendRawTransaction(signed.rawTransaction)
-        print(result.hex())
-        return result.hex()
-    else:
-        tx_data = create_tx(coin,account,recipient,amount)
-        signed = account.sign_transaction(tx_data)
-        NetworkAPI.broadcast_tx_testnet(signed)
-        return signed
-
+# # send_tx(ETH, eth_account, str(eth_to_acc), 1)
+# send_tx(BTC, btc_account, eth_to_acc, 1)
 
